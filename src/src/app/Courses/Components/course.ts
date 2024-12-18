a80d29b60d7f142be0cf3da11b686573a7cb1b0c
sharmilapadhy23 <issa.23052005@gmail.com>
2024-12-18 19:40:21 +0530

    commit.

---
+++ src/src/app/Courses/Components/course.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-course',
+  templateUrl: './create-course.component.html',
+  styleUrls: ['./create-course.component.css']
+})
+export class CreateCourseComponent {
+  // Add any logic here if needed for the form submission
+  constructor() {}
+}
